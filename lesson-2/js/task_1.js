d9291791b5543950474a64f4477ac6c7b4880247
Daria Steblovska <darias1896.96@gmail.com>
2025-10-17 02:06:38 +0100

    ts config file set up

--- lesson-2/js/task_1.js
+++ lesson-2/js/task_1.js
@@ -1,2 +1,10 @@
+"use strict";
 //---- Задача 1. Вводиться перший номер  місяця якоїсь пори року (3,6,9,12). Визначити пору року. Передбачити перевірку і генерувати помилку якщо некоректний місяць (1-12) і генерувати помилку якщо це не перший місяць пори року. Використати never.
-
+const monthNum = parseInt(prompt("Month number"));
+function throwError(message) {
+    throw new Error(message);
+}
+// try {
+//   const monthNum:number = parseInt(prompt("Month number")!)
+// } catch (error) {
+// }
---
+++ lesson-2/src/task_1.ts
@@ -0,0 +1,13 @@
+//---- Задача 1. Вводиться перший номер  місяця якоїсь пори року (3,6,9,12). Визначити пору року. Передбачити перевірку і генерувати помилку якщо некоректний місяць (1-12) і генерувати помилку якщо це не перший місяць пори року. Використати never.
+
+const monthNum:number = parseInt(prompt("Month number")!)
+
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+// try {
+//   const monthNum:number = parseInt(prompt("Month number")!)
+// } catch (error) {
+  
+// }
