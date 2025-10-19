aa4c2e054e1d63739337b6c6ea371b683c17ea9a
Daria Steblovska <darias1896.96@gmail.com>
2025-10-20 00:14:43 +0100

    task 2 solved and added

--- lesson-2/src/task_2.ts
+++ lesson-2/src/task_2.ts
@@ -1,2 +1,22 @@
 //---- Задача 2. Створити функцію, яка дозволяє знайти або останню цифру числа, або останній символ числа.
 
+// const userVal = prompt('Enter anything')
+
+function getLastSymbol(val: number): number;
+function getLastSymbol(val: string): string;
+
+function getLastSymbol(val: number | string): number | string {
+  if (typeof val === "number") return val % 10;
+  if (typeof val === "string") return val[val.length - 1];
+  else throw new Error("No value");
+}
+
+const num = 12345
+const str = 'Hello'
+
+document.write(`Last symbol of <b>${num}</b> - ${getLastSymbol(num).toString()}<br/>`);
+document.write(`Last symbol of <b>${str}</b> - ${getLastSymbol("Hello")}<br/>`);
+
+// const h = 'hello'
+// document.write(h.slice(-1));
+// document.write(h.at(-1)!);
