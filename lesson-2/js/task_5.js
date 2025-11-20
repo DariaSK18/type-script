9e28840ff8b56f6dcf000258181cb2e47d34c153
Daria Steblovska <darias1896.96@gmail.com>
2025-11-20 04:18:26 +0000

    task 5 solved

--- lesson-2/js/task_5.js
+++ lesson-2/js/task_5.js
@@ -1,2 +1,29 @@
 "use strict";
 //---- Задача 5. Дано набір налаштувань (ключ-значення(enabled/disabled)). Вивести ті, які є увімкненими
+var SettingStatus;
+(function (SettingStatus) {
+    SettingStatus["Enabled"] = "enabled";
+    SettingStatus["Disabled"] = "disabled";
+})(SettingStatus || (SettingStatus = {}));
+const settings = {
+    notifications: SettingStatus.Enabled,
+    darkMode: SettingStatus.Disabled,
+    autoSave: SettingStatus.Enabled,
+    locationAccess: SettingStatus.Disabled,
+    backup: SettingStatus.Enabled
+};
+function getEnabledSettings(settings) {
+    const enabledSettings = [];
+    for (const key in settings) {
+        if (settings[key] === 'enabled') {
+            enabledSettings.push(key);
+        }
+    }
+    return enabledSettings;
+}
+function displaySettings(settings) {
+    const enabledSettings = getEnabledSettings(settings);
+    document.write(`Enabled settings for : <br>`);
+    enabledSettings.forEach((el, i) => document.write(`${i + 1}. ${el}<br>`));
+}
+displaySettings(settings);
--- lesson-2/src/task_5.ts
+++ lesson-2/src/task_5.ts
@@ -1,2 +1,38 @@
 //---- Задача 5. Дано набір налаштувань (ключ-значення(enabled/disabled)). Вивести ті, які є увімкненими
 
+enum SettingStatus {
+    Enabled = 'enabled',
+    Disabled = 'disabled'
+}
+
+type Settings = {
+    [key: string]: SettingStatus
+}
+
+const settings: Settings = {
+    notifications: SettingStatus.Enabled,
+    darkMode: SettingStatus.Disabled,
+    autoSave: SettingStatus.Enabled,
+    locationAccess: SettingStatus.Disabled,
+    backup: SettingStatus.Enabled
+}
+
+function getEnabledSettings(settings: Settings): string [] {
+    const enabledSettings: string[] = []
+
+    for(const key in settings) {
+        if(settings[key] === 'enabled'){
+            enabledSettings.push(key)
+        }
+    }
+    return enabledSettings
+}
+
+function displaySettings(settings: Settings): void {
+    const enabledSettings: string[] = getEnabledSettings(settings)
+    document.write(`Enabled settings for : <br>`)
+    enabledSettings.forEach((el,i) => document.write(`${i+1}. ${el}<br>`))
+}
+
+displaySettings(settings)
+
